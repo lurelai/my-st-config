86f84ec3a94080643b9b33808d2618351a2c8b3b
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-16 15:54:02 -0300

    patch vertcenter

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.3;
+static float chscale = 1.1;
 
 /*
  * word delimiter string
